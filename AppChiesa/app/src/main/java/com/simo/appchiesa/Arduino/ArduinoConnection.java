8e19b19ee86099ab5a56728c81f7ae41f235a01a
Simo <simobenzi97@gmail.com>
2019-10-17 14:39:59 +0200

    Aggiunto aggiornamento stato circuito luce

--- AppChiesa/app/src/main/java/com/simo/appchiesa/Arduino/ArduinoConnection.java
+++ AppChiesa/app/src/main/java/com/simo/appchiesa/Arduino/ArduinoConnection.java
@@ -1,29 +1,14 @@
 package com.simo.appchiesa.Arduino;
 
-import android.app.DownloadManager;
 import android.content.Context;
-import android.os.AsyncTask;
 import android.util.Log;
 
-import com.android.volley.AuthFailureError;
 import com.android.volley.Request;
 import com.android.volley.RequestQueue;
 import com.android.volley.Response;
 import com.android.volley.VolleyError;
 import com.android.volley.toolbox.StringRequest;
 import com.android.volley.toolbox.Volley;
-
-import org.json.JSONException;
-import org.json.JSONObject;
-
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStream;
-import java.io.InputStreamReader;
-import java.net.URI;
-import java.net.URISyntaxException;
-import java.util.HashMap;
-import java.util.Map;
 
 public class ArduinoConnection {
     //declare variable needed
@@ -41,11 +26,42 @@ public class ArduinoConnection {
         this.requestCommand = requestCommand;
     }
 
-    public void sendRequest() {
+    public void scenarioActivation(String scenario) {
 
+
+    }
+
+    public void scenarioADeactivation(String scenario) {
+
+
+    }
+
+    public void lightActivation(String lightCircuit){
         // Instantiate the RequestQueue
         RequestQueue queue = Volley.newRequestQueue(this.context);
-        String url = "http://192.168.1.124/5/off:80";
+        String url = "http://192.168.1.124/" + lightCircuit + "/on:80";
+
+        // Request a string response from the provided URL
+        StringRequest request = new StringRequest(Request.Method.GET, url,
+                new Response.Listener<String>() {
+                    @Override
+                    public void onResponse(String response) {
+                        Log.e("onResponse", response.substring(0, 50));
+                    }
+                }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                Log.e("onErrorResponse", "That didn't work!");
+            }
+        });
+        // Add the request to the RequestQueue.
+        queue.add(request);
+    }
+
+    public void lightDeactivation(String lightCircuit){
+        // Instantiate the RequestQueue
+        RequestQueue queue = Volley.newRequestQueue(this.context);
+        String url = "http://192.168.1.124/" + lightCircuit + "/off:80";
 
         // Request a string response from the provided URL
         StringRequest request = new StringRequest(Request.Method.GET, url,
---
+++ AppChiesa/app/src/main/java/com/simo/appchiesa/LightCircuits/LightCircuits.java
@@ -0,0 +1,83 @@
+package com.simo.appchiesa.LightCircuits;
+
+import android.content.Context;
+import android.content.SharedPreferences;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+
+import com.google.gson.Gson;
+import com.google.gson.reflect.TypeToken;
+
+import java.lang.reflect.Type;
+import java.util.ArrayList;
+
+import static android.content.Context.MODE_PRIVATE;
+
+public class LightCircuits {
+
+    public ArrayList<String> ArrayLights;     // 0(CH.4) 1(CH.3) 2(CH.4) 3(CH.5) 4(CH.6a) 5(CH.6b) 6(CH.9)  7(CH.10) 8(CH.11) 9(CH.12a) 10(CH.12b) 11(CH.15)
+
+    public LightCircuits(){
+        this.ArrayLights = new ArrayList<String>();
+    }
+
+    // salva gli stati delle luci
+    public static void SaveLights(ArrayList<String> arrayLights, Context context) {
+
+        SharedPreferences sharedPreferences = context.getSharedPreferences("Lights", MODE_PRIVATE);
+        SharedPreferences.Editor editor = sharedPreferences.edit();
+        Gson gson = new Gson();
+        String json = gson.toJson(arrayLights);
+        editor.putString("Lights", json);
+        editor.commit();
+
+    }
+
+    //prende gli stati salvati delle luci
+    public static ArrayList<String> loadLights(Context context) {
+        SharedPreferences sharedPreferences = context.getSharedPreferences("Lights", MODE_PRIVATE);
+        Gson gson = new Gson();
+        String json = sharedPreferences.getString("Lights", null);
+        Type type = new TypeToken<ArrayList<String>>() {
+        }.getType();
+        ArrayList<String> arrayLightsLoaded = gson.fromJson(json, type);
+        if (arrayLightsLoaded == null) {     //se é la prima volta che si avvia l'app vengono creati gli impostazioni
+            arrayLightsLoaded = new ArrayList<String>();
+            arrayLightsLoaded.add(0, "off");
+            arrayLightsLoaded.add(1, "off");
+            arrayLightsLoaded.add(2, "off");
+            arrayLightsLoaded.add(3, "off");
+            arrayLightsLoaded.add(4, "off");
+            arrayLightsLoaded.add(5, "off");
+            arrayLightsLoaded.add(6, "off");
+            arrayLightsLoaded.add(7, "off");
+            arrayLightsLoaded.add(8, "off");
+            arrayLightsLoaded.add(9, "off");
+            arrayLightsLoaded.add(10, "off");
+            arrayLightsLoaded.add(11, "off");
+
+            return arrayLightsLoaded;
+        } else
+            return arrayLightsLoaded;
+    }
+
+    public static Bitmap setImageLights (String string, Context context)  {
+
+        int intImg;
+        Bitmap newImage;
+
+        if(string.contains(" ")){
+            string = string.replace(" ","_");
+        }
+        else
+            string = string;
+
+
+        intImg = context.getResources().getIdentifier("luce_" +
+                string.toLowerCase(), "mipmap", context.getPackageName()); //salva l'id della bitmap corretta
+
+        newImage = BitmapFactory.decodeResource(context.getResources(), intImg);
+
+        return newImage;
+    }
+}
